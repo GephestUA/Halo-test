c27710107b53301ab850516b6b6d7c0eba4ba7e5
GephestUA <gephestua@gmail.com>
2021-06-29 18:41:40 +0300

    change deriktori muve to APP and deleted RefisterForm component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import styles from './App.module.scss';
 import ProgressBar from './components/ProgressBar/ProgressBar';
-import RegistrationPage from './components/RegistationPage/RegistrationPage.jsx';
+import React from 'react';
+import UserInfo from './components/UserInfo/UserInfo';
+import ServiceInfo from './components/ServiceInfo/ServiceInfo.jsx';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className={styles.appWrapper}>
-      <ProgressBar />
-      <RegistrationPage />
-    </div>
+    <Router>
+      <div className={styles.appWrapper}>
+        <ProgressBar />
+        <Switch>
+          <Route exact path="/" render={() => <UserInfo />} />
+          <Route path="/stage2" render={() => <ServiceInfo />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
