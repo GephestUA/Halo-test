0db642b17d3448c50bf683732d4994dade73f165
GephestUA <gephestua@gmail.com>
2021-07-02 21:05:16 +0300

    yet done 2 page

--- src/components/UserInfo/ButtonService/ButtonSendForm.jsx
+++ src/components/UserInfo/ButtonService/ButtonSendForm.jsx
@@ -3,7 +3,7 @@ import s from './ButtonSendForm.module.scss';
 
 export default function ButtonService(props) {
   return (
-    <div className={s.button}>
+    <div className={s.button} onClick={props.handleClickButton}>
       <input type="submit" value={props.btnName} disabled={props.btnStatus}></input>
     </div>
   );
