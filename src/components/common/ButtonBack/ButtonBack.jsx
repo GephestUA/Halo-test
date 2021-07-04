73ec036d1946381fef6db94b700f77ba01a356a9
GephestUA <gephestua@gmail.com>
2021-07-04 17:50:04 +0300

    modified button back

--- src/components/common/ButtonBack/ButtonBack.jsx
+++ src/components/common/ButtonBack/ButtonBack.jsx
@@ -1,12 +1,25 @@
-import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import { getStageInfo } from '../../../store/app-controller/action-creators';
 import styles from './ButtonBack.module.scss';
 
-function ButtonBack(props) {
+function ButtonBack() {
   const history = useHistory();
+  const location = useLocation();
+
+  const stage = useSelector((state) => state.appControls.stage);
+  const dispatch = useDispatch();
+
+  const currentURL = location.pathname;
+
+  const handelBackClick = () => {
+    history.push(`/stage${currentURL[currentURL.length - 1] - 1}`);
+    dispatch(getStageInfo(stage - 1));
+  };
 
   return (
     <div className={styles.buttonBlock}>
-      <button type="button" onClick={() => history.goBack()} className={styles.button}></button>
+      <button type="button" onClick={handelBackClick} className={styles.button}></button>
     </div>
   );
 }
