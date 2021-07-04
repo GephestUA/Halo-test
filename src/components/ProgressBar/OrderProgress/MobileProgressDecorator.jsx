30c71db9e4cb116760e971742bc16b20ab0412e9
GephestUA <gephestua@gmail.com>
2021-07-04 19:49:16 +0300

    added text name to mobile progress

--- src/components/ProgressBar/OrderProgress/MobileProgressDecorator.jsx
+++ src/components/ProgressBar/OrderProgress/MobileProgressDecorator.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import mobileStyles from './MobileProgressDecorator.module.scss';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function OrderProgress({ disabled, text, success, navigate }) {
   let disabledLine = disabled && mobileStyles.disabledLine;
+  const selectedService = useSelector((state) => state.services.serviceSelected);
+  const stage = useSelector((state) => state.appControls.stage);
+  const changeText = stage > 2 && text === 'Service';
+
   return (
     <NavLink
       to={`${navigate}`}
@@ -11,7 +16,7 @@ export default function OrderProgress({ disabled, text, success, navigate }) {
       disabled={disabled}
       data-success={success}
     >
-      <p>{text}</p>
+      <p>{changeText ? selectedService : text}</p>
     </NavLink>
   );
 }
