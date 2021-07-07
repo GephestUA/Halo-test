ebf697665cef30a190635a956335bb6f6a1b2561
GephestUA <gephestua@gmail.com>
2021-07-07 20:48:39 +0300

    Logo to Link and resetStage

--- src/components/ProgressBar/Logo/Logo.jsx
+++ src/components/ProgressBar/Logo/Logo.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import MaskGroup from '../../../assets/image/svg/MaskGroup.svg';
 import styles from './Logo.module.scss';
 import ButtonBack from '../../common/ButtonBack/ButtonBack';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, Link } from 'react-router-dom';
+import { resetStage } from '../../../store/app-controller/action-creators';
 
 export default function Logo() {
   const stage = useSelector((state) => state.appControls.stage);
+  const dispatch = useDispatch();
   const location = useLocation();
 
   return (
     <div className={styles.logoBlock}>
       {stage > 1 && location.pathname !== '/stage1' && <ButtonBack />}
       <div>
-        <a href="/stage1">
-          <img src={MaskGroup} width="36px" height="32px" alt="logo" />
-        </a>
+        <Link to="/stage1">
+          <img src={MaskGroup} width="36px" height="32px" alt="logo" onClick={dispatch(resetStage)} />
+        </Link>
       </div>
     </div>
   );
